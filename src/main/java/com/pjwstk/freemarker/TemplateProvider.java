ec4534f1ad32f18308c97bded00e93ddcee106c0
MaciejDzieciuch <macidzie@gmail.com>
2019-11-12 16:31:45 +0100

    Prepare configuration to manage freemarker templates

---
+++ src/main/java/com/pjwstk/freemarker/TemplateProvider.java
@@ -0,0 +1,25 @@
+package com.pjwstk.freemarker;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import java.io.IOException;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.servlet.ServletContext;
+
+@RequestScoped
+public class TemplateProvider {
+
+    private static final String TEMPLATES_DIRECTORY_PATH = "WEB-INF/fm-templates";
+
+    @Inject
+    private ConfigProvider configProvider;
+
+    public Template getTemplate(ServletContext servletContext, String templateName)
+        throws IOException {
+
+      Configuration configuration = configProvider.getConfiguration();
+      configuration.setServletContextForTemplateLoading(servletContext, TEMPLATES_DIRECTORY_PATH);
+      return configuration.getTemplate(templateName);
+    }
+}
--- src/main/java/com/pjwstk/servlet/HomeServlet.java
+++ src/main/java/com/pjwstk/servlet/HomeServlet.java
@@ -1,6 +1,13 @@
 package com.pjwstk.servlet;
 
+import com.pjwstk.freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,10 +17,23 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
 
+  @Inject
+  private TemplateProvider templateProvider;
+
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
 
+    Template template = templateProvider.getTemplate(getServletContext(), "home.ftlh");
 
+    Map<String, Object> dataModel = new HashMap<>();
+
+    PrintWriter printWriter = resp.getWriter();
+
+    try {
+      template.process(dataModel, printWriter);
+    } catch (TemplateException e) {
+      e.printStackTrace();
+    }
   }
 }
