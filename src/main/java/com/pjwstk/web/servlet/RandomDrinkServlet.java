607b535c2991b8375cd1f0e8f13d05bd0d571f4d
MaciejDzieciuch <macidzie@gmail.com>
2020-01-15 15:37:48 +0100

    Reformat code

--- src/main/java/com/pjwstk/web/servlet/RandomDrinkServlet.java
+++ src/main/java/com/pjwstk/web/servlet/RandomDrinkServlet.java
@@ -7,7 +7,6 @@ import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
